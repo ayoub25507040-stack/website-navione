d4e39d0b64906516a634b22e4501ed45768ee8df
ayoub25507040-stack <ayoub25507040@gmail.com>
2026-07-07 15:41:56 +0100

    Cache-busting sur css/style.css et js/app.js (+ imports controllers)
    Cause racine des "ca ne s'affiche pas sur mon telephone" repetes :
    GitHub Pages sert ces fichiers avec Cache-Control: max-age=600, et les
    navigateurs mobiles n'ont pas de recharge forcee facile comme sur
    desktop -> l'ancienne version restait en cache jusqu'a 10 minutes apres
    chaque deploiement. Ajoute un parametre ?v= sur les references
    principales (CSS, JS, et les imports de chaque controller) pour forcer
    un fetch frais a chaque changement de cette version.

--- js/app.js
+++ js/app.js
@@ -8,19 +8,19 @@
  *
  * This file just wires every Controller to the page once it's parsed.
  */
-import * as PageTransitionController from './controllers/PageTransitionController.js';
-import * as PreloaderController from './controllers/PreloaderController.js';
-import * as LanguageController from './controllers/LanguageController.js';
-import * as ModalController from './controllers/ModalController.js';
-import * as SolutionModalController from './controllers/SolutionModalController.js';
-import * as CursorGlowController from './controllers/CursorGlowController.js';
-import * as AboutTiltController from './controllers/AboutTiltController.js';
-import * as HeroTiltController from './controllers/HeroTiltController.js';
-import * as ScrollController from './controllers/ScrollController.js';
-import * as NavController from './controllers/NavController.js';
-import * as RevealController from './controllers/RevealController.js';
-import * as CounterController from './controllers/CounterController.js';
-import * as TabsController from './controllers/TabsController.js';
+import * as PageTransitionController from './controllers/PageTransitionController.js?v=202607071540';
+import * as PreloaderController from './controllers/PreloaderController.js?v=202607071540';
+import * as LanguageController from './controllers/LanguageController.js?v=202607071540';
+import * as ModalController from './controllers/ModalController.js?v=202607071540';
+import * as SolutionModalController from './controllers/SolutionModalController.js?v=202607071540';
+import * as CursorGlowController from './controllers/CursorGlowController.js?v=202607071540';
+import * as AboutTiltController from './controllers/AboutTiltController.js?v=202607071540';
+import * as HeroTiltController from './controllers/HeroTiltController.js?v=202607071540';
+import * as ScrollController from './controllers/ScrollController.js?v=202607071540';
+import * as NavController from './controllers/NavController.js?v=202607071540';
+import * as RevealController from './controllers/RevealController.js?v=202607071540';
+import * as CounterController from './controllers/CounterController.js?v=202607071540';
+import * as TabsController from './controllers/TabsController.js?v=202607071540';
 
 function init() {
   PageTransitionController.init();
